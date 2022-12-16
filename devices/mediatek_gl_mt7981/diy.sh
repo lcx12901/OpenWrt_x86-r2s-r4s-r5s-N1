75922a72130fde0f591ff412779fdde95dfc7161
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2022-12-16 11:08:23 +0800

    update

--- devices/mediatek_gl_mt7981/diy.sh
+++ devices/mediatek_gl_mt7981/diy.sh
@@ -8,7 +8,7 @@ kernel_v="$(cat include/kernel-version.mk | grep LINUX_KERNEL_HASH-5.* | cut -f 
 echo "KERNEL=${kernel_v}" >> $GITHUB_ENV || true
 sed -i "s?targets/%S/.*'?targets/%S/$kernel_v'?" include/feeds.mk
 
-rm -rf package/feeds/kiddin9/{firewall,rtl88x2bu}
+rm -rf package/feeds/kiddin9/{firewall,rtl88x2bu,base-files,netifd}
 
 rm -rf devices/common/patches/{glinet,imagebuilder.patch,iptables.patch,targets.patch,kernel-defaults.patch,disable_flock.patch}
 
@@ -20,3 +20,4 @@ mv -f target/linux/mediatek target/linux/mediatek_gl
 
 
 
+连感冒药都没做储备更别提其他医疗资源,就突然完全放开,躺匪真的让人受不了
